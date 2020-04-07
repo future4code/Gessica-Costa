b046f63e3b08ac1c469f71cfc9fad1524f6487dc
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-07 15:27:25 -0300

    ifs alterados para switch

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -3,58 +3,64 @@ function adicionarTarefa() {
     const semana = document.getElementById("diasSemana")
     const novaTarefa = tarefa.value
     const diaSemana = semana.value
+    let listaTarefas = ""
     tarefa.value = ""
 
     if (novaTarefa === " " || novaTarefa == "") {
         alert("Insira um item")
-    } else if (diaSemana === "segunda") {
-        const listaTarefas = document.getElementById("lista-segunda")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-    } else if (diaSemana === "terca") {
-        const listaTarefas = document.getElementById("lista-terca")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-    } else if (diaSemana === "quarta") {
-        const listaTarefas = document.getElementById("lista-quarta")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-    } else if (diaSemana === "quinta") {
-        const listaTarefas = document.getElementById("lista-quinta")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-    } else if (diaSemana === "sexta") {
-        const listaTarefas = document.getElementById("lista-sexta")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-    } else if (diaSemana === "sabado") {
-        const listaTarefas = document.getElementById("lista-sabado")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-    } else if (diaSemana === "domingo") {
-        const listaTarefas = document.getElementById("lista-domingo")
-        listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+    } else {
+        switch (diaSemana) {
+            case "segunda":
+                listaTarefas = document.getElementById("lista-segunda");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+            case "terca":
+                listaTarefas = document.getElementById("lista-terca");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+            case "quarta":
+                listaTarefas = document.getElementById("lista-quarta");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+            case "quinta":
+                listaTarefas = document.getElementById("lista-quinta");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+            case "sexta":
+                listaTarefas = document.getElementById("lista-sexta");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+            case "sabado":
+                listaTarefas = document.getElementById("lista-sabado");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+            case "domingo":
+                listaTarefas = document.getElementById("lista-domingo");
+                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                break;
+        }
+        // if (diaSemana === "segunda") {
+        //     const listaTarefas = document.getElementById("lista-segunda")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // } else if (diaSemana === "terca") {
+        //     const listaTarefas = document.getElementById("lista-terca")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // } else if (diaSemana === "quarta") {
+        //     const listaTarefas = document.getElementById("lista-quarta")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // } else if (diaSemana === "quinta") {
+        //     const listaTarefas = document.getElementById("lista-quinta")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // } else if (diaSemana === "sexta") {
+        //     const listaTarefas = document.getElementById("lista-sexta")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // } else if (diaSemana === "sabado") {
+        //     const listaTarefas = document.getElementById("lista-sabado")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // } else if (diaSemana === "domingo") {
+        //     const listaTarefas = document.getElementById("lista-domingo")
+        //     listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
+        // }
     }
+
 }
-
-
-
-// switch (diaSemana) {
-//     case "segunda":
-//         const listaTarefas = document.getElementById("lista-segunda")
-//         listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-//         break;
-//     case "terca":
-//         const listaTarefas = document.getElementById("lista-terca")
-//         listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>"
-//         break;
-// }
-
-// function adicionarTarefa() {
-//     const input = document.querySelector("input")
-//     const novoItem = input.value
-
-//     if (novoItem === "") {
-//         alert("Insira um item!!!")
-//     } else {
-//         const listaDeCompras = document.querySelector("ul")
-
-//         listaDeCompras.innerHTML += "<li>" + novoItem + "</li>"
-
-//         input.value = ""
-//     }
-// }
