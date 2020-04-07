c128ac7a9d13ae6d37b02e86f8c1f4b6b67bde0a
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-07 16:36:08 -0300

    Adicionei que as tarefas devem ser riscadas quando clicadas.

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -12,37 +12,41 @@ function adicionarTarefa() {
         switch (diaSemana) {
             case "segunda":
                 listaTarefas = document.getElementById("lista-segunda");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
             case "terca":
                 listaTarefas = document.getElementById("lista-terca");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
             case "quarta":
                 listaTarefas = document.getElementById("lista-quarta");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
             case "quinta":
                 listaTarefas = document.getElementById("lista-quinta");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
             case "sexta":
                 listaTarefas = document.getElementById("lista-sexta");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
             case "sabado":
                 listaTarefas = document.getElementById("lista-sabado");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
             case "domingo":
                 listaTarefas = document.getElementById("lista-domingo");
-                listaTarefas.innerHTML += "<li>" + novaTarefa + "</li>";
+                listaTarefas.innerHTML += "<li onclick='riscar(this)'>" + novaTarefa + "</li>";
                 break;
         }
     }
 }
 
+function riscar(elemento) {
+    elemento.style.textDecoration = "line-through"
+}
+
 function apagarTarefas() {
     for (let i = 0; i < 7; i++)
-        document.getElementsByTagName("UL")[i].innerHTML = ""
+        document.getElementsByTagName("ul")[i].innerHTML = ""
 }
