52802e7af143ee09cee1dae02ab384e9dcde24f2
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-08 21:39:42 -0300

    Posts inserido no HTML.

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,20 +1,23 @@
-const posts = []
+let posts = []
+let i = posts.length
 
-function formulario(evento) {
-    const titulo = document.getElementById("titulo")
-    const autor = document.getElementById("autor")
-    const conteudo = document.getElementById("conteudo")
-    const valorTitulo = titulo.value
-    const valorAutor = autor.value
-    const valorConteudo = conteudo.value
+
+function formulario() {
+
+    let titulo = document.getElementById("titulo")
+    let autor = document.getElementById("autor")
+    let conteudo = document.getElementById("conteudo")
+    let valorTitulo = titulo.value
+    let valorAutor = autor.value
+    let valorConteudo = conteudo.value
+    let postagens = document.getElementById("postagens")
 
     posts.push({ titulo: valorTitulo, autor: valorAutor, conteudo: valorConteudo })
-    console.log(posts)
+    console.log(i)
+
+    postagens.innerHTML += "<article><p>" + posts[i].titulo + "</p><p>" + posts[i].autor +
+        "</p><p>" + posts[i].conteudo + "</article>"
 
     titulo.value = autor.value = conteudo.value = ""
-}
-
-function adicionarPost(titulo, autor, conteudo) {
-    const postagens = document.getElementById("postagens")
-    postagens.innerHTML += "<article><h4>" + titulo + "</h4><h5>" + autor + "</h5><p>" + conteudo + "</p></article>"
+    i = posts.length
 }
