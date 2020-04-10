7d454ba7436819d75ef63cb077a8588350265774
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-10 17:56:18 -0300

    Conversor de moeda.

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,16 @@
+/* Projeto da semana
+
+    Aula 14:
+
+    Exercícios de leitura de código
+
+    1. A função que converte um valor em dolar para reais, considerando a cotação
+    informada pelo usuário.
+    A entrada da função é o valor em dolar, dentro do escopo da função é
+    declarada uma variável do tipo const que recebe um valor informado pelo
+    usuário e converte para Number. A função retorna o valor em reais da multiplicação
+    do valor em dolar pela cotação.
+    Fora da função, o valor de entrada declarado é 100, e então a
+    saída da função é impressa no console.
+    O valor impresso depende do valor informado pelo usuário, se for 5, derá impresso 500.
+*/
