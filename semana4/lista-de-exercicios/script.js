ce3d492e8dbd6fcac9950844010a811aa8492e71
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-10 17:57:22 -0300

    Investe dinheiro.

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -13,4 +13,16 @@
     Fora da função, o valor de entrada declarado é 100, e então a
     saída da função é impressa no console.
     O valor impresso depende do valor informado pelo usuário, se for 5, derá impresso 500.
+
+    2. A função recebe duas entradas, o tipo de investimento e o valor a ser investido,
+    e retorna o valor após o investimento.
+    No escopo da função é declarada uma variável do tipo let. Dependendo do tipo de investimento
+    escolhido na entrada, o valor será multiplicado por uma porcentagem, e a função retornará
+    esse valor. Cada valor está informado em cada case do switch.
+    Fora da função foram declaradas duas variáveis do tipo const, que chamam a função com
+    os valores de entrada. Depois esses valores são impressos no console.
+    O primeiro valor impresso será 165, teve um rendimento de 10% de 150.
+    O segundo valor será o alert "TIPO DE INVESTIMENTO INFORMADO INCORRETO!", porque nenhum
+    dos cases do switch tem "Tesouro Direto", então é retornado o valor do default.
+
 */
