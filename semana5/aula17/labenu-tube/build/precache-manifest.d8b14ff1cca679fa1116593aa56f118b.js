5639ab85a519f7c438c1972b2c3cb8188f6f6d92
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-13 15:37:03 -0300

    Utilizei react para criação do lab-tube.

---
+++ semana5/aula17/labenu-tube/build/precache-manifest.d8b14ff1cca679fa1116593aa56f118b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e99ecb5423f9e66901e2dc538e33da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ca69d1e3f675f6b25a7",
+    "url": "/static/css/main.b7eb40fd.chunk.css"
+  },
+  {
+    "revision": "ea1635aadbe94c383d30",
+    "url": "/static/js/2.3293cdf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3293cdf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca69d1e3f675f6b25a7",
+    "url": "/static/js/main.87e67775.chunk.js"
+  },
+  {
+    "revision": "a3a4330c5ac65ed83bb3",
+    "url": "/static/js/runtime-main.8179f348.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
