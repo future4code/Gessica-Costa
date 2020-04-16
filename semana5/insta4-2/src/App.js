5c4fe630a369ca6e662ac6bc355b17e5cbfba3e7
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-16 16:53:25 -0300

    Coloquei os Posts em um objeto.

--- semana5/insta4-2/src/App.js
+++ semana5/insta4-2/src/App.js
@@ -3,24 +3,40 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    pessoas: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50?a=1',
+        fotoPost: 'https://picsum.photos/200/150?a=2'
+      },
+      {
+        nomeUsuario: 'joao',
+        fotoUsuario: 'https://picsum.photos/50/50?a=3',
+        fotoPost: 'https://picsum.photos/200/150?a=4'
+      },
+      {
+        nomeUsuario: 'pedro',
+        fotoUsuario: 'https://picsum.photos/50/50?a=5',
+        fotoPost: 'https://picsum.photos/200/150?a=6'
+      }
+    ]
+  }
   render() {
+
+    const listaDePosts = this.state.pessoas.map((pessoa, index) => {
+      return (
+        <Post key = {index}
+          nomeUsuario={pessoa.nomeUsuario}
+          fotoUsuario={pessoa.fotoUsuario}
+          fotoPost={pessoa.fotoPost}
+        />
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=2'}
-        />
-        <Post
-          nomeUsuario={'joao'}
-          fotoUsuario={'https://picsum.photos/50/50?a=3'}
-          fotoPost={'https://picsum.photos/200/150?a=4'}
-        />
-        <Post
-          nomeUsuario={'pedro'}
-          fotoUsuario={'https://picsum.photos/50/50?a=5'}
-          fotoPost={'https://picsum.photos/200/150?a=6'}
-        />
+        {listaDePosts}
       </div>
     );
   }
