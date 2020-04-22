d0488bcd461a83aec91e97d74247a503977ebda0
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-04-22 16:37:59 -0300

    Lista de Tarefas com LocalStorage.

---
+++ semana6/boiler-plate/build/precache-manifest.173cea6d9ee0f6b4814e0dcc08fe2efa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1e8b9bcdd2a73eee30a8e460891249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a41eb50af9855022a312",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "a41eb50af9855022a312",
+    "url": "/static/js/main.b21624b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
