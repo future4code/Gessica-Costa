bbc4f68c915ad15710d1b104753cdd350adc792c
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-05-12 19:22:24 -0300

    Integração com API utilizando get Profile To Choose e Choose Person.

--- semana9/astromatch-projeto/src/components/TelaInicial.js
+++ semana9/astromatch-projeto/src/components/TelaInicial.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Styled from 'styled-components'
 import axios from 'axios'
 
@@ -25,6 +25,9 @@ const Label = Styled.label`
     align-self: center;
 `
 const Image = Styled.img`
+    max-width: 360px;
+    max-height: 300px;
+    border-radius: 1%;
 `
 const ContainerBody = Styled.div`
     padding: 20px;
@@ -36,23 +39,63 @@ const ButtonPositive = Styled.button``
 
 export function TelaInicial(props) {
 
+    const [list, setList] = useState({})
+
+    useEffect(() => {
+        axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${gessica-costa-julian}/person',
+        {
+            aluno: 'gessica-costa-julian'
+        })
+            .then(res => {
+                console.log(res.data.profile)
+                setList(res.data.profile)
+            })
+            .catch(err => {
+                console.log('Erro no get profile', err)
+            })
+    }, [])
+
+    const deuMatch = id => {
+        console.log('id', id)
+        console.log('clicou')
+        const body = {
+            "id": id,
+            "choice": true
+        }
+
+        axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person",
+            body,
+            {
+                headers: {'Content-Type': "application/json"}
+            },
+            {
+                aluno: 'gessica-costa-julian'
+            })
+            .then(res => {
+                console.log(res)
+            }).catch(err => {
+                console.log('Erro no choose person', err)
+            })
+    }
+
     return (
         <ContainerInicial>
             <ContainerHeader>
                 <Header>
                     <Label>astro<b>match</b></Label>
-                    <Button onClick={props.onClick}>Metches</Button>
+                    <Button onClick={props.onClick}>Matches</Button>
                 </Header>
             </ContainerHeader>
             <ContainerBody>
                 <Information>
-                    <Image src={"https://picsum.photos/360/200"} />
-                    <h5>Nome, idade</h5>
-                    <p>Descrição</p>
+                    <Image src={list.photo} alt="Foto" />
+                    <h5>{list.name}</h5>
+                    <label>{list.age}</label>
+                    <p>{list.bio}</p>
                 </Information>
                 <Choice>
                     <ButtonNegative>X</ButtonNegative>
-                    <ButtonPositive>S2</ButtonPositive>
+                    <ButtonPositive onClick={() => deuMatch(list.id)}>S2</ButtonPositive>
                 </Choice>
             </ContainerBody>
         </ContainerInicial>
