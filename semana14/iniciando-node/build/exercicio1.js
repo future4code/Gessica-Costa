816fc492954952c3af1125f88ead0baefa744a44
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-06-23 14:34:48 -0300

    Nome e idade informados pelo usuário.

---
+++ semana14/iniciando-node/build/exercicio1.js
@@ -0,0 +1,5 @@
+"use strict";
+var name = process.argv[2];
+var age = Number(process.argv[3]);
+console.log("Ol\u00E1, " + name + "! Voc\u00EA tem " + age + " anos. Em sete anos voc\u00EA ter\u00E1 " + (age + 7) + " anos.");
+//# sourceMappingURL=exercicio1.js.map
