cd9c028047f2ec0ed84fc2842f40bfa4d92084e9
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-07-27 16:31:45 -0300

    Testes da função verifyAge

--- semana19/testes-automatizados/src/verifyAge.ts
+++ semana19/testes-automatizados/src/verifyAge.ts
@@ -1,48 +1,115 @@
-export const verifyAge = (casino: Casino, users: User[]): Result => {
-    let result: Result
+// export const verifyAge = (casino: Casino, users: User[]): Result => {
+//     let allowedBrazilian: string[] = []
+//     let allowedAmerican: string[] = []
+//     let unallowedBrazilian: string[] = []
+//     let unallowedAmerican: string[] = []
+//     let allowed: string[] = []
+//     let unallowed: string[] = []
 
-    users && users.map((u: any) => {
-        if (u.nacionality = NACIONALITY.AMERICAN) {
-            u.age >= 21 ?
-                result.american.allowed.push(u)
-                : result.american.unallowed.push(u)
-        } else if (u.nacionality = NACIONALITY.BRAZILIAN) {
-            u.age >= 18 ?
-                result.brazilian.allowed.push(u)
-                : result.brazilian.unallowed.push(u)
+//     users && users.map((u: any) => {
+//         if (casino.location === LOCATION.BRAZIL) {
+//             u.age >= 18 ?
+//                 allowed.push(u.name)
+//                 : unallowed.push(u.name)
+//         } else if (casino.location = LOCATION.EUA) {
+//             (u.age >= 21) ?
+//                 allowed.push(u.name)
+//                 : unallowed.push(u.name)
+//         }
+//     })
+
+//     allowed && allowed.map((u: any) => {
+//         u.nacionality === NACIONALITY.BRAZILIAN ?
+//             allowedBrazilian.push(u)
+//             : allowedAmerican.push(u)
+//     })
+
+//     unallowed && unallowed.map((u: any) => {
+//         u.nacionality === NACIONALITY.BRAZILIAN ?
+//             unallowedBrazilian.push(u)
+//             : unallowedAmerican.push(u)
+//     })
+
+//     return {
+//         brazilian: {
+//             allowed: allowedBrazilian,
+//             unallowed: unallowedBrazilian
+//         },
+//         american: {
+//             allowed: allowedAmerican,
+//             unallowed: unallowedAmerican
+//         }
+//     }
+// }
+
+export function verifyAge(casino: Casino, users: User[]): Result {
+    const allowed: User[] = [];
+    const unallowed: User[] = [];
+  
+    for (const user of users) {
+      if (casino.location === LOCATION.EUA) {
+        if (user.age >= 21) {
+          allowed.push(user);
+        } else {
+          unallowed.push(user);
         }
-    })
+      } else if (casino.location === LOCATION.BRAZIL) {
+        if (user.age >= 18) {
+          allowed.push(user);
+        } else {
+          unallowed.push(user);
+        }
+        break;
+      }
+    }
+  
+    return {
+      brazilian: {
+        allowed: allowed
+          .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+          .map((u) => u.name),
+        unallowed: unallowed
+          .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+          .map((u) => u.name),
+      },
+      american: {
+        allowed: allowed
+          .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+          .map((u) => u.name),
+        unallowed: unallowed
+          .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+          .map((u) => u.name),
+      },
+    };
+  }
 
-    return result
-}
-
-interface User {
+export interface User {
     name: string,
     age: number,
     nacionality: NACIONALITY
 }
 
-interface Casino {
+export interface Casino {
     name: string,
     location: LOCATION
 }
 
-interface Result {
+export interface Result {
     brazilian: ResultItem
     american: ResultItem
 }
 
-interface ResultItem {
+export interface ResultItem {
     allowed: string[]
     unallowed: string[]
 }
 
-enum LOCATION {
+export enum LOCATION {
     EUA = "EUA",
-    BRASIL = "BRAZIL"
+    BRAZIL = "BRAZIL"
 }
 
-enum NACIONALITY {
+export enum NACIONALITY {
     BRAZILIAN = "BRAZILIAN",
     AMERICAN = "AMERICAN"
 }
