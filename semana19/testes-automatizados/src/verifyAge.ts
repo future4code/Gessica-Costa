ef08a7432a9e9928dd8d88036b961c57db0b1b31
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-07-27 22:12:06 -0300

    Comentários apagados.

--- semana19/testes-automatizados/src/verifyAge.ts
+++ semana19/testes-automatizados/src/verifyAge.ts
@@ -42,47 +42,6 @@ export const verifyAge = (casino: Casino, users: User[]): Result => {
   }
 }
 
-// export function verifyAge(casino: Casino, users: User[]): Result {
-//     const allowed: User[] = [];
-//     const unallowed: User[] = [];
-
-//     for (const user of users) {
-//       if (casino.location === LOCATION.EUA) {
-//         if (user.age >= 21) {
-//           allowed.push(user);
-//         } else {
-//           unallowed.push(user);
-//         }
-//       } else if (casino.location === LOCATION.BRAZIL) {
-//         if (user.age >= 18) {
-//           allowed.push(user);
-//         } else {
-//           unallowed.push(user);
-//         }
-//         break;
-//       }
-//     }
-
-//     return {
-//       brazilian: {
-//         allowed: allowed
-//           .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
-//           .map((u) => u.name),
-//         unallowed: unallowed
-//           .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
-//           .map((u) => u.name),
-//       },
-//       american: {
-//         allowed: allowed
-//           .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
-//           .map((u) => u.name),
-//         unallowed: unallowed
-//           .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
-//           .map((u) => u.name),
-//       },
-//     };
-//   }
-
 export interface User {
   name: string,
   age: number,
