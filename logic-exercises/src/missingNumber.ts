9195fb28294ef92b72a2681655b58d0ff8995e60
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-09-04 18:49:25 -0300

    Criado teste para função missingNumber.

--- logic-exercises/src/missingNumber.ts
+++ logic-exercises/src/missingNumber.ts
@@ -4,21 +4,17 @@ export const missingNumber = (input: number[]): number => {
     let orderedInput: number[] = [];
     let oneHunderOrdered: number[] = [];
     let missingNumber: number = 0;
-
+    
     for (let number = 1; number <= 100; number++) {
         oneHunderOrdered.push(number);
     }
-
     orderedInput = input.sort(function (a, b) {
         return a - b;
     })
-
     for (const comparativeNumber of oneHunderOrdered) {
         if (orderedInput.indexOf(comparativeNumber) === -1) {
             missingNumber = comparativeNumber;
         }
     }
-
-
     return missingNumber;
 }
